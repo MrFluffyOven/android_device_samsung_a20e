5a3f4d1bc710bb29b85610cd89b6e0248dd714f2
Jamie Falconer <mrfluffyoven@gmail.com>
2024-10-29 09:10:33 +0000

    upload some twrp-11 things

--- device.mk
+++ device.mk
@@ -5,10 +5,4 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a20e.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_a20e-user \
-    twrp_a20e-userdebug \
-    twrp_a20e-eng
+LOCAL_PATH := device/samsung/a20e
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,10 @@
-add_lunch_combo omni_a20e-eng
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo twrp_a20e-user
+add_lunch_combo twrp_a20e-userdebug
+add_lunch_combo twrp_a20e-eng
